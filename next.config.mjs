dfdc47b4d4e3cf1bf7a0509ee2267b569c484cb1
Paolo <p.chiaramello@gmail.com>
2025-12-16 17:38:18 +0100

    config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   env: {
     // INCOLLA QUI LE TUE CHIAVI TRA LE VIRGOLETTE
     // Esempio: NEXT_PUBLIC_SUPABASE_URL: "https://..."
