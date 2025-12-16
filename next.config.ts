e935e78eee6f8fb56d9e5d201f46275f1b9c49a0
Paolo <p.chiaramello@gmail.com>
2025-12-16 15:46:52 +0100

    chiavi integrate

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    // INCOLLA QUI LE TUE CHIAVI TRA LE VIRGOLETTE
+    // Esempio: NEXT_PUBLIC_SUPABASE_URL: "https://..."
+    NEXT_PUBLIC_SUPABASE_URL: "https://awsbpmvpaaruvlzzzivr.supabase.co",
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3c2JwbXZwYWFydXZsenp6aXZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1NDMzMTcsImV4cCI6MjA4MTExOTMxN30.6OSeQn1ghjHeLWyevv4QbO6I1M1VN9Y8rhk4jHK10TM",
+  },
 };
 
 export default nextConfig;
